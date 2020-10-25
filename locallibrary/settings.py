4b394250e091beb0a63f382295983001e097f31b
maddypie <66778010+maddypie@users.noreply.github.com>
2020-10-26 00:19:14 +0530

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '3athw%6r!z*vl-*$8dn=wy*%i&@)zi=33up928fa&dnm5j#*d#'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3athw%6r!z*vl-*$8dn=wy*%i&@)zi=33up928fa&dnm5j#*d#')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
-ALLOWED_HOSTS = ['guarded-retreat-67309.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['books-at-mylib.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
